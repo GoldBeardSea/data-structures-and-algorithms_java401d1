18f893a97483658f1d9c04f01ac809bcb5e7bcbb
GoldBeardSea <timbusch1@gmail.com>
2018-07-18 10:00:19 -0700

    needs tests still

---
+++ LL_k_from_end/src/LinkedListTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @org.junit.jupiter.api.Test
+    void kthFromEnd() {
+        // Will set up test, instantiate a linked list, test for .data of return of node from kthFromEnd
+    }
+
+    @org.junit.jupiter.api.Test
+    void append() {
+    }
+}
