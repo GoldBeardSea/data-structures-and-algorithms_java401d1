bdd9553d13deffaea55cd12869c6e13c921b683f
GoldBeardSea <timbusch1@gmail.com>
2018-07-21 23:42:31 -0700

    committing tests

--- LL_k_from_end/src/LinkedListTest.java
+++ LL_k_from_end/src/LinkedListTest.java
@@ -3,11 +3,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class LinkedListTest {
 
     @org.junit.jupiter.api.Test
-    void kthFromEnd() {
-        // Will set up test, instantiate a linked list, test for .data of return of node from kthFromEnd
+    void kthFromEndOne() {
+        LinkedList testList = new LinkedList();
+        testList.append(40);
+        testList.append(10);
+        testList.append(19);
+        testList.append(13);
+        testList.append(15);
+        testList.append(222);
+        ListNode test = testList.kthFromEnd(5);
+        assertEquals(15, test.data);
     }
 
     @org.junit.jupiter.api.Test
-    void append() {
+    void kthFromEndKthLongerThanList() {
+        LinkedList testList = new LinkedList();
+        testList.append(40);
+        testList.append(10);
+        testList.append(19);
+        testList.append(13);
+        testList.append(15);
+        testList.append(222);
+        ListNode test = testList.kthFromEnd(10);
+        assertEquals(222, test.data);
     }
+
+    @org.junit.jupiter.api.Test
+    void kthFromEndKthEmptyList() {
+        LinkedList testList = new LinkedList();
+        ListNode test = testList.kthFromEnd(5);
+        assertNull(test);
+    }
+
+
 }
