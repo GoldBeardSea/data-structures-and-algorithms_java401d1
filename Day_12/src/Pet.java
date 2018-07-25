124c8cd59d77cc0b14c51061806169cf294c25f5
GoldBeardSea <timbusch1@gmail.com>
2018-07-25 12:26:33 -0700

    still working on code problem with return statement

---
+++ Day_12/src/Pet.java
@@ -0,0 +1,18 @@
+public class Pet {
+    public String name;
+    public String sound;
+
+
+    public Pet(String name) {
+    this.name = name;
+}
+
+    public String toString() {
+        return this.name;
+    }
+
+    public String sound() {
+        return "";
+    }
+}
+
